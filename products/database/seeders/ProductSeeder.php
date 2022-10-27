cc06023130ccb5dd98e023b6d1996093e6eb5a26
SergejsRom <sergejsromanovskis@gmail.com>
2022-10-27 22:49:22 +0300

    init structure

---
+++ products/database/seeders/ProductSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+ 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+
+            'SKU' => str_random(10),
+            'name' => str_random(10),
+            'price' => str_random(10),
+            'attributes' => str_random(10),
+            'values' => str_random(10),
+        ]);
+    }
+}
