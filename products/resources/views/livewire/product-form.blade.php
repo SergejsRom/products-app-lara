b8a8d21fd2260f693879e821d369c1c8bb3b63a1
SergejsRom <sergejsromanovskis@gmail.com>
2022-11-07 16:42:45 +0200

    wire form

--- products/resources/views/livewire/product-form.blade.php
+++ products/resources/views/livewire/product-form.blade.php
@@ -1,3 +1,37 @@
 <div>
-    The whole world belongs to you.
+    <form action="{{ route('products.store') }}" method="post" class="flex">
+                        
+        @csrf
+        <div class="col-8">
+        SKU:
+        <input class="form-control m-1" required type="text" name="SKU">
+        Name:
+        <input class="form-control m-1" required type="text" name="name">
+        Price:
+        <input class="form-control m-1" required type="number" name="price">
+        Attributes:
+        <select class="form-control m-1" class="form-control" name="attribute">
+
+            {{-- <option value="{{$att}}" @selected('Select')>{{$att}}</option> --}}
+            
+                
+        </select>
+        Values:
+            
+        
+        {{-- <?php
+            $attributes = match ($att) {
+                "DVD" => ["MB"],
+                "Furniture" => ["W", "H", "L"],
+                "Books" => ["Weight"],
+                default => [],
+            };
+            echo "Values:";
+            foreach ($attribute as $att => $attribute)
+            echo "<input class='form-control m-1' required type='text' name='value'>";
+        ?> --}}
+        
+        <button type="submit" class="btn btn-primary m-2">Save</button>
+    </div>
+    </form>
 </div>
