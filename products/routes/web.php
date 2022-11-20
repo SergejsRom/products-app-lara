ccd31e3d96eacd68342f1ee81f44424b2adbf88f
SergejsRom <sergejsromanovskis@gmail.com>
2022-11-20 21:39:03 +0200

    .

--- products/routes/web.php
+++ products/routes/web.php
@@ -27,5 +27,5 @@ Route::resource('products', Product::class);
 Auth::routes();
 
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
